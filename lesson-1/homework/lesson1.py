314e138de1c68a4bcd8ba125e52b820d50281b79
tamirlan0707 <t.sadikdjanov@gmail.com>
2025-10-18 16:07:52 +0500

    Refactor lesson1.py for tasks and calculations

--- lesson-1/homework/lesson1.py
+++ lesson-1/homework/lesson1.py
@@ -1,36 +1,32 @@
 # lesson1
 
-# task-1 Given a side of square. Find its perimeter and area.
+# task1
+a = 6
+perimeter = 4*a
+area = a ** 2
+print("Perimeter:",perimeter)
+print("Area:",area)
 
-side = float(input("Укажите ширину квадрата: "))
-perimetr = 4 * side
-area = side ** 2
-print(f"Перимитр квадрата: {perimetr}")
-print(f"Площадь квадрата: {area}")
-
-# task-2 Given diameter of circle. Find its length.
-
+# # task2
+d = 5
 import math
-diameter = float(input("Введите диаметр круга: "))
-length = math.pi * diameter
-print(f"Длина окужности: {length}")
+length = math.pi*d
+print("Length of circle:", length)
 
-# task-3 Given two numbers a and b. Find their mean.
-
-a = float(input("Введите первое число: "))
-b = float(input("Введите второе число: "))
+# # task3
+a = 6
+b = 10
 mean = (a+b) / 2
-print(f"Среднее значение: {mean}")
+print("Mean:",mean)
 
-# task-4 Given two numbers a and b. Find their sum, product and square of each number.
-
-a = float(input("Введите первое число: "))
-b = float(input("Введите второе число: "))
-sum_ab = a+b
-product_ab = a*b
-square_a = a ** 2
-square_b = b ** 2
-print(f"Сумма: {sum_ab}")
-print(f"Произведение: {product_ab}")
-print(f"Квадрат первого числа: {square_a}")
-print(f"Квадрат второго числа: {square_b}")
+# task4
+a = 4
+b = 7
+sum_result = a+b
+product = a*b
+square_a = a**2
+square_b = b**2
+print("Sum:",sum_result)
+print("Product:",product)
+print("Square of a:",square_a)
+print("Square of b:",square_b)
